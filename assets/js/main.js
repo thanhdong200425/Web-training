e11cffa7e8fdeb62327a253be78e6351e236a706
Dong <thanhdong200425@gmail.com>
2023-08-04 14:32:17 +0700

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-    showSlides(slideIndex += n);  
-}
+// let slideIndex = 1;
+// showSlides(slideIndex);
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);  
+// }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mn__slideshow__slides__slide");
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("mn__slideshow__slides__slide");
 
-    if(n > slides.length) {
-        slideIndex = 1;
-    }
-    if( n < 1) {
-        slideIndex = slides.length;
-    }
+//     if(n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if( n < 1) {
+//         slideIndex = slides.length;
+//     }
 
-    for(i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if(slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex-1].style.display = "flex";
-    setTimeout(showSlides, 1000);
-}
+//     for(i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if(slideIndex > slides.length) {
+//         slideIndex = 1;
+//     }
+//     slides[slideIndex-1].style.display = "flex";
+//     setTimeout(showSlides, 1000);
+// }
